911785a1d5c85d75595e8d14798a8437b6d4e56f
A. Matías Quezada <amatiasq@gmail.com>
2023-12-05 11:26:39 +0100

    Optimizations

--- lanterfish.ts
+++ lanterfish.ts
@@ -1,27 +1,24 @@
 const FISH_REPRODUCTION_CYCLE = 6;
 const NEWBORN_MATURITY = 2;
 
-function iterate(input) {
-  const output: number[] = [];
-  const newFish: number[] = [];
+function iterate(input: number[]) {
+  const length = input.length;
 
-  for (const fishCycle of input) {
+  for (let i = 0; i < length; i++) {
+    const fishCycle = input[i];
+
     if (fishCycle === 0) {
-      output.push(FISH_REPRODUCTION_CYCLE);
-      newFish.push(FISH_REPRODUCTION_CYCLE + NEWBORN_MATURITY);
+      input[i] = FISH_REPRODUCTION_CYCLE;
+      input.push(FISH_REPRODUCTION_CYCLE + NEWBORN_MATURITY);
     } else {
-      output.push(fishCycle - 1);
+      input[i] = fishCycle - 1;
     }
   }
-
-  return [...output, ...newFish];
 }
 
-const generations = [[3, 4, 3, 1, 2]];
+const fish = [3, 4, 3, 1, 2];
 
 for (let i = 0; i < 2000; i++) {
-  const last = generations.at(-1);
-  const newGeneration = iterate(last);
-  generations.push(newGeneration);
-  console.log(newGeneration.length);
+  iterate(fish);
+  console.log(i, fish.length);
 }
