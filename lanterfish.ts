862c08754ef944705c6d895c3e64de43c0eafa04
A. Matías Quezada <amatiasq@gmail.com>
2023-12-05 12:04:13 +0100

    Use object instead of array

--- lanterfish.ts
+++ lanterfish.ts
@@ -1,24 +1,35 @@
 const FISH_REPRODUCTION_CYCLE = 6;
 const NEWBORN_MATURITY = 2;
 
-function iterate(input: number[]) {
-  const length = input.length;
+function iterate(input: Record<number, number>) {
+  const result = {};
 
-  for (let i = 0; i < length; i++) {
-    const fishCycle = input[i];
-
-    if (fishCycle === 0) {
-      input[i] = FISH_REPRODUCTION_CYCLE;
-      input.push(FISH_REPRODUCTION_CYCLE + NEWBORN_MATURITY);
-    } else {
-      input[i] = fishCycle - 1;
+  for (let i = 0; i <= FISH_REPRODUCTION_CYCLE + NEWBORN_MATURITY; i++) {
+    if (i !== 0) {
+      result[i - 1] = input[i] ?? 0;
     }
   }
+
+  result[FISH_REPRODUCTION_CYCLE] += input[0] ?? 0;
+  result[FISH_REPRODUCTION_CYCLE + NEWBORN_MATURITY] = input[0];
+  return result;
+}
+
+function groupFish(fish: number[]) {
+  const result = {};
+
+  for (const entry of fish) {
+    result[entry] = (result[entry] ?? 0) + 1;
+  }
+
+  return result;
 }
 
 const fish = [3, 4, 3, 1, 2];
+let grouppedFish = groupFish(fish);
+console.log(0, grouppedFish);
 
 for (let i = 0; i < 2000; i++) {
-  iterate(fish);
-  console.log(i, fish.length);
+  grouppedFish = iterate(grouppedFish);
+  console.log(i, grouppedFish);
 }
